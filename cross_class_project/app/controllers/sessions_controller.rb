61289f5f99b7f457b4f6e77b406831ddee88856a
jbudrow <jbudrowd3@yahoo.com>
2016-04-06 17:17:54 -0400

    temporarily comment out before_action authenticate

--- cross_class_project/app/controllers/sessions_controller.rb
+++ cross_class_project/app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
-  before_action :authenticate!
+  # before_action :authenticate!
 
   def create
     @user = User.find_by!(email: params['email'])
     if @user.authenticate(params['password'])
       session[:user_id] = @user.id
-      render json: { @user.as_json [:email]},
+      render json: { user: @user.as_json(only: [:email]) },
              status: :ok
+      # render json: { user: @user.as_json }
     else
       render json: { message: "Invalid email or password." },
              status: :unauthorized
