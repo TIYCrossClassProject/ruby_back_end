59b5210459798badc27fcade85706d5d07d62de0
jbudrow <jbudrowd3@yahoo.com>
2016-04-06 23:10:15 -0400

    remove csrf from image controller

--- cross_class_project/app/controllers/users_controller.rb
+++ cross_class_project/app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
 
-  def create 
+  def create
     @user = User.new(alias: params['alias'],
                      first: params['first'],
                      last: params['last'],
                      email: params['email'],
                      password: params['password'])
     if @user.save
-      render json: { user: @user.as_json(only: [:first_name, :last_name, :email, :auth_token ]) },
+      render json: { user: @user.as_json(only: [:first_name, :last_name, :email]) },
                      status: :created
     else
       render json: { errors: @user.errors.full_messages },
