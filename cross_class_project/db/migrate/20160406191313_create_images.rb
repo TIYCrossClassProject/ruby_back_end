31e590e28659b4f27abb393857fe19c84bdfb96d
DaveKeith <d.keith78@yahoo.com>
2016-04-07 14:09:20 -0400

    replaced duplicate gem 'pg' with gem 'aws-sdk'

--- cross_class_project/db/migrate/20160406191313_create_images.rb
+++ cross_class_project/db/migrate/20160406191313_create_images.rb
@@ -2,7 +2,9 @@ class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
       t.integer :user_id
-      t.string :url
+      t.string :image_file_name
+      t.string :image_content_type
+      t.integer :image_file_size
 
       t.timestamps null: false
     end
