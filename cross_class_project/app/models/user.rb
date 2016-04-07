5ff1340657c29f98e3d8bb505a74bad316a22a51
jbudrow <jbudrowd3@yahoo.com>
2016-04-07 16:18:58 -0400

    implement auth token method to models

--- cross_class_project/app/models/user.rb
+++ cross_class_project/app/models/user.rb
@@ -1,8 +1,24 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates_presence_of :email
-  validates :email, format: {
-            with: /.+@.+\..+/,  message: 'Invalid email address.'
-          }
+  validates :auth_token, presence: true
+  validates :email, presence: true, uniqueness: true,
+    format: {
+      with: /.+\@.+\..+/,
+      message: "Invalid email addresss"
+    }
+
+  def ensure_auth_token
+    unless self.auth_token
+      self.auth_token = User.generate_token
+    end
+  end
+
+  def self.generate_token
+    token = SecureRandom.hex
+    while User.exists?(auth_token: token)
+      token = SecureRandom.hex
+    end
+    token
+  end
 end
