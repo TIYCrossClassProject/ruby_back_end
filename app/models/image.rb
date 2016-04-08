bf495778618fb0cf019cf4d07ab51d1fc90cecd8
jbudrow <jbudrowd3@yahoo.com>
2016-04-07 21:19:11 -0400

    add paperclip validations to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,10 +2,9 @@ class Image < ActiveRecord::Base
   belongs_to :user
   has_many :answers
 
-  has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
   validates :answer, presence: true
-
   validates_presence_of :user_id
+
+  has_attached_file :image
 end
