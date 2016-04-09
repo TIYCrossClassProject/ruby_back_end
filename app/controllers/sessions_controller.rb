a10782ec98df4ed73dab1739699fd7bc3362bdd8
jbudrow <jbudrowd3@yahoo.com>
2016-04-09 12:08:20 -0400

    left #TODO reminder user avatar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,23 +2,22 @@ class SessionsController < ApplicationController
   before_action :authenticate!, only: [:check_auth]
 
   def create
-    binding.pry
     @user = User.find_by!(email: params['email'])
     if @user.authenticate(params['password'])
       render json: { user: @user.as_json(only: [:email, :auth_token]) },
              status: :ok
     else
-      render json: { message: "Invalid email or password." },
+      render json: { message: 'Invalid email or password.' },
              status: :unauthorized
     end
   end
 
   def check_auth
     if logged_in_user
-      render json: { message: "Logged in as #{logged_in_user.email}" },
+      render json: { message: "Successfully logged in as #{logged_in_user.email}." },
              status: :ok
     else
-      render json: { message: "Auth isn't working yet." },
+      render json: { message: 'Invalid authorization.' },
              status: :unauthorized
     end
   end
