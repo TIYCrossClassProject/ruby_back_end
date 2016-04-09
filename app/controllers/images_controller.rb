0c1d459b57ee69b2f3cfe4093028c9fe6c37fffe
jbudrow <jbudrowd3@yahoo.com>
2016-04-09 12:07:26 -0400

    pluarlize images to file name

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,13 @@
+class ImagesController < ApplicationController
+  before_action :authenticate!, only: [:create]
+
+  def create
+    @image = logged_in_user.images.create(logo: params['file'])
+
+    if @image.save
+      render 'create.json.jbuilder', status: :created
+    else
+      render json: { errors: @image.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
