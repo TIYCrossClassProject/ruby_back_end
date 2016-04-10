14d942f9894f6eca66087c647d98db4434df5173
jbudrow <jbudrowd3@yahoo.com>
2016-04-10 14:52:38 -0400

    throw in answer params for image create method

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,8 @@ class ImagesController < ApplicationController
   before_action :authenticate!, only: [:create]
 
   def create
-    @image = logged_in_user.images.create(logo: params['file'])
+    @image = logged_in_user.images.create(logo: params['file'],
+                                          answer: params['answer'])
 
     if @image.save
       render 'create.json.jbuilder', status: :created
