93a02293cccd40d66fa214712b0784738a9885e7
jbudrow <jbudrowd3@yahoo.com>
2016-04-10 12:41:49 -0400

    create a controller for guesses, implement a create method

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,2 +1,21 @@
 class GuessesController < ApplicationController
+  before_action :authenticate!, only: [:create]
+
+  def create
+    @guess = logged_in_user.guesses.create(field: params['field'],
+                                           image_id: params['id'])
+  #   if @guess.image.answer == @guess.field
+  #     render 'answer.json.jbuilder', status: :created
+  #   elsif @guess.field
+  #     render 'create.json.jbuilder', status: :created
+  #   else
+  #     render json: { errors: @guess.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
+    if @guess.field
+      render 'answer.json.jbuilder', status: :created
+    else
+      render json: { errors: @guess.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
