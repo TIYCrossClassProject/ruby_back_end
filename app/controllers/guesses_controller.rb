263a90e995285e4f0e39ccb2fd99058f9e3772fa
jbudrow <jbudrowd3@yahoo.com>
2016-04-10 13:59:27 -0400

    tweak guess create conditional for correct/incorrect guesses

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -2,20 +2,19 @@ class GuessesController < ApplicationController
   before_action :authenticate!, only: [:create]
 
   def create
-    @guess = logged_in_user.guesses.create(field: params['field'],
-                                           image_id: params['id'])
-  #   if @guess.image.answer == @guess.field
-  #     render 'answer.json.jbuilder', status: :created
-  #   elsif @guess.field
-  #     render 'create.json.jbuilder', status: :created
-  #   else
-  #     render json: { errors: @guess.errors.full_messages }, status: :unprocessable_entity
-  #   end
-  # end
-    if @guess.field
-      render 'answer.json.jbuilder', status: :created
-    else
-      render json: { errors: @guess.errors.full_messages }, status: :unprocessable_entity
-    end
+    @guess = logged_in_user.guesses.new(field: params['field'],
+                                        image_id: params['id'])
+    @image = Image.find_by(id: params['id'])
+
+    if @guess.save
+      if @guess.image.answer == @guess.field
+        @image.update(solution: @guess.id)
+        render 'answer.json.jbuilder', status: :created
+      elsif @guess.field
+        render 'create.json.jbuilder', status: :created
+      else
+       render json: { errors: @guess.errors.full_messages }, status: :unprocessable_entity
+     end
+   end
   end
 end
