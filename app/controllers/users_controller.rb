d51abb5aa73df3553d9b3143dfe0092bfd4fd664
jbudrow <jbudrowd3@yahoo.com>
2016-04-10 20:25:10 -0400

    add missing param avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
                      password: params['password'])
     @user.ensure_auth_token
     if @user.save
-      render json: { user: @user.as_json(only: [:alias, :first, :last, :email, :auth_token]) },
+      render json: { user: @user.as_json(only: [:alias, :avatar, :first, :last, :email, :auth_token]) },
                      status: :created
     else
       render json: { errors: @user.errors.full_messages },
